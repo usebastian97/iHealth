8d03615dfb4ef07c64afa9c4a733a552a756c3a0
SebastianUtoiu <s.utoiu@plintech.ro>
2023-02-19 11:34:14 +0200

    add RecyclerView for CategoriesFragment.kt

---
+++ app/src/main/java/usg/ihealth/ui/home/CategoriesAdapter.kt
@@ -0,0 +1,49 @@
+package usg.ihealth.ui.home
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import usg.ihealth.R
+
+class CategoriesAdapter(
+    private val categoriesList: List<CategoriesSection>,
+    private val context: Context
+) : RecyclerView.Adapter<CategoriesAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val imageView: ImageView = itemView.findViewById(R.id.ivCategories)
+        val textView: TextView = itemView.findViewById(R.id.tvCategories)
+
+    }
+
+
+    //-- Inflate the design for items in RecyclerView --//
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CategoriesAdapter.ViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.categories_items, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: CategoriesAdapter.ViewHolder, position: Int) {
+        val itemCategories = categoriesList[position]
+        holder.imageView.setImageResource(itemCategories.img)
+        holder.textView.text = itemCategories.title
+
+
+    }
+
+    //-- Initialize the list of items --//
+    override fun getItemCount(): Int {
+        return categoriesList.size
+    }
+
+    data class CategoriesSection(val title: String, val img: Int)
+}
