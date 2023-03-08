701d64c4be4e26e48544ffdad0913a1bfa98b3ec
SebastianUtoiu <s.utoiu@plintech.ro>
2023-03-08 17:06:43 +0200

    add nav to BmiFragment

--- app/src/main/java/usg/ihealth/ui/fragments/BmiFragment.kt
+++ app/src/main/java/usg/ihealth/ui/fragments/BmiFragment.kt
@@ -5,23 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import usg.ihealth.databinding.FragmentSettingsBinding
-import usg.ihealth.ui.viewModels.SettingsViewModel
+import usg.ihealth.databinding.FragmentBmiBinding
+import usg.ihealth.ui.viewModels.BmiViewModel
 
-class SettingsFragment : Fragment() {
+class BmiFragment : Fragment() {
 
-    private lateinit var viewModel: SettingsViewModel
-    private var _binding: FragmentSettingsBinding? = null
+    private lateinit var viewModel: BmiViewModel
+
+    private var _binding: FragmentBmiBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        _binding = FragmentBmiBinding.inflate(inflater, container, false)
 
         return binding.root
     }
-
 
 }
--- app/src/main/java/usg/ihealth/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/usg/ihealth/ui/fragments/DashboardFragment.kt
@@ -10,8 +10,8 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import usg.ihealth.R
 import usg.ihealth.databinding.FragmentDashboardBinding
-import usg.ihealth.ui.viewModels.DashboardViewModel
 import usg.ihealth.ui.adapters.DashboardAdapter
+import usg.ihealth.ui.viewModels.DashboardViewModel
 
 class DashboardFragment : Fragment() {
 
@@ -108,7 +108,9 @@ class DashboardFragment : Fragment() {
     }
 
     private fun navigateToBmi() {
-        TODO("Not yet implemented")
+        val action =
+            DashboardFragmentDirections.navigationToBmi()
+        findNavController().navigate(action)
     }
 
     private fun navigateToProfile() {
