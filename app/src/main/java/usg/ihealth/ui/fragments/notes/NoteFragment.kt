d1fa037e896968b07af4de6bcefaed6995545e1e
SebastianUtoiu <s.utoiu@plintech.ro>
2023-03-27 16:38:02 +0300

    update NoteFragment & add navigation for this fragment.

--- app/src/main/java/usg/ihealth/ui/fragments/notes/NoteFragment.kt
+++ app/src/main/java/usg/ihealth/ui/fragments/notes/NoteFragment.kt
@@ -5,17 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import usg.ihealth.R
+import usg.ihealth.databinding.FragmentNoteBinding
 
 class NoteFragment : Fragment() {
 
+    private var _binding: FragmentNoteBinding? = null
     private lateinit var viewModel: NoteViewModel
+
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_note, container, false)
+    ): View {
+        _binding = FragmentNoteBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
 }
